9d38245d91f8b5d047b0dd55f18293b4ffdab470
Akulllvlllad <Akulllvlllad@gmail.com>
2025-01-16 16:17:51 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const { sendVote } = require('./api.js')
+const app = express()
+const port = 3000
+
+
+app.use(express.static('public'))
+
+app.get('/api/send',(req, res) => {
+  const { variant } = req.query
+  if(variant){
+    sendVote(variant)
+  }
+  res.redirect('/?status=ok')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
